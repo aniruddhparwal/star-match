fa4930913523d83442a09793e1d57d715ced240e
aniruddhparwal <aniruddhparwal@gmail.com>
2020-04-21 20:01:15 +0530

    Updates

---
+++ precache-manifest.9f57b9d36de76111da29af1da8bb9bef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "444df29d10491d46bb87288d599c64e1",
+    "url": "/star-match/index.html"
+  },
+  {
+    "revision": "60202e73dbfc9a1aafe2",
+    "url": "/star-match/static/css/main.9336e814.chunk.css"
+  },
+  {
+    "revision": "52f36e5375209ca17a09",
+    "url": "/star-match/static/js/2.59502b98.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/star-match/static/js/2.59502b98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60202e73dbfc9a1aafe2",
+    "url": "/star-match/static/js/main.ee439212.chunk.js"
+  },
+  {
+    "revision": "262456c65261bf46727b",
+    "url": "/star-match/static/js/runtime-main.ab270d40.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/star-match/static/media/logo.5d5d9eef.svg"
+  }
+]);
